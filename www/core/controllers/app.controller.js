a97fb5889f6ac97f9addc4c2856676c635a16475
Emmanuel Pautet <emmanuel.pautet@gmail.com>
2017-06-10 14:11:00 -0300

    select city

--- www/core/controllers/app.controller.js
+++ www/core/controllers/app.controller.js
@@ -1,4 +1,20 @@
 angular.module('starter')
-    .controller('AppController', function () {
+    .controller('AppController', function ($rootScope) {
 
+        $rootScope.cities = [{
+            id: 1,
+            name: "João Pessoa",
+            state: "PB"
+        }, {
+            id: 2,
+            name: "Salvador",
+            state: "BA"
+        }, {
+            id: 3,
+            name: "São Luis",
+            state: "MA"
+        }
+        ];
+
+        $rootScope.currentCity = $rootScope.cities[0];
     });
--- www/home/controllers/home.controller.js
+++ www/home/controllers/home.controller.js
@@ -1,9 +1,53 @@
 angular.module('starter')
-    .controller('HomeCtrl', [ '$scope' , '$state' , function ( $scope , $state ) {
-        
-        $scope.onBtnReclamacao = function ( ) {
+    .controller('HomeCtrl', ['$scope','$rootScope', '$state', '$ionicModal', function ($scope, $rootScope, $state, $ionicModal) {
+
+    
+
+        $scope.onBtnReclamacao = function () {
             console.log('click');
             $state.go('app.categoriasReclamacoes');
         }
+
+        $scope.onOpenChooseCity = function () {
+            console.log('click');
+            $scope.modal.show();
+        }
+
+
+        $ionicModal.fromTemplateUrl('my-modal.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
+        
+
+        $scope.cancelChooseCity = function () {
+            $scope.modal.hide();
+        };
+
+        $scope.selectCity = function ( city ) {
+            $rootScope.currentCity = city;
+        }
+
+        
+        $scope.saveChooseCity = function () {
+            $scope.modal.hide();
+        };
+
+
+        // Cleanup the modal when we're done with it!
+        $scope.$on('$destroy', function () {
+            $scope.modal.remove();
+        });
+        // Execute action on hide modal
+        $scope.$on('modal.hidden', function () {
+            // Execute action
+        });
+        // Execute action on remove modal
+        $scope.$on('modal.removed', function () {
+            // Execute action
+        });
+
 
     }]);
