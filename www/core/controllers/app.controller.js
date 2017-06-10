d26728798861132bbd98c8506a13320816c4d822
Emmanuel Pautet <emmanuel.pautet@gmail.com>
2017-06-10 15:41:39 -0300

    commit

--- www/core/controllers/app.controller.js
+++ www/core/controllers/app.controller.js
@@ -17,4 +17,41 @@ angular.module('starter')
         ];
 
         $rootScope.currentCity = $rootScope.cities[0];
+
+
+        $rootScope.categories = [ {
+            id: 1,
+            name: 'Saúde',
+            icon: 'fa-medkit'
+        },
+        {
+            id: 2,
+            name: 'Infraestrutura',
+            icon: 'fa-building'
+        },
+        {
+            id: 3,
+            name: 'Educação',
+            icon: 'fa-graduation-cap'
+        },
+        {
+            id: 4,
+            name: 'Justiça',
+            icon: 'fa-gavel'
+        },
+        {
+            id: 5,
+            name: 'Segurança',
+            icon: 'fa-shield'
+        },
+        {
+            id: 6,
+            name: 'Administração',
+            icon: 'fa-university'
+        },
+        {
+            id: 7,
+            name: 'Transito',
+            icon: 'fa-automobile'
+        }]
     });
