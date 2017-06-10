421cab83a212b9334683724a61bdaf0fc6ef74ac
Emmanuel Pautet <emmanuel.pautet@gmail.com>
2017-06-10 20:05:02 -0300

    commit

--- www/reclamacoes/controllers/subcategorias-reclamacoes.controller.js
+++ www/reclamacoes/controllers/subcategorias-reclamacoes.controller.js
@@ -1,5 +1,8 @@
 angular.module('starter')
-    .controller('SubCategoriasReclamacoesCtrl', [ '$scope' ,'$state' , '$stateParams' , function ( $scope, $state, $stateParams ) {
+    .controller('SubCategoriasReclamacoesCtrl', [ '$scope' ,'$rootScope' , '$state' , '$stateParams' , 'lodash', function ( $scope, $rootScope, $state, $stateParams, lodash ) {
 
-        $scope.currentCategory = $stateParams.id;
+        var id = Number($stateParams.id);
+        $scope.currentCategory = lodash.find($rootScope.categories, { 'id': id });
+
+        console.log( lodash.reverse($rootScope.categories), +$stateParams.id);
     }]);
