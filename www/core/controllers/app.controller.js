ce9f0fea36011ebc50540e9161237060e97257b5
Emmanuel Pautet <emmanuel.pautet@gmail.com>
2017-06-11 12:14:21 -0300

    commit page

--- www/core/controllers/app.controller.js
+++ www/core/controllers/app.controller.js
@@ -33,7 +33,7 @@ angular.module('starter')
         });
         
 
-        $rootScope.respostas = [{
+        $rootScope.reclamacoes = [{
             id:1,
             title: "Tem buracos na rua",
             description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Aliquam erat volutpat. Nunc auctor. Mauris pretium quam et urna. Fusce nibh. Duis risus. Curabitur sagittis hendrerit',
