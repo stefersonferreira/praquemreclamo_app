a1ced6a7b8740a7315e42406125f7eefb34fac55
Emmanuel Pautet <emmanuel.pautet@gmail.com>
2017-06-11 12:10:25 -0300

    commit

--- www/reclamacoes/controllers/reclamacoes-list.controller.js
+++ www/reclamacoes/controllers/reclamacoes-list.controller.js
@@ -1,23 +1,25 @@
 angular.module('starter')
-    .controller('ReclamacoesListCtrl', ['$scope', '$rootScope', '$state', '$http',
-        function ($scope, $rootScope, $state, $http) {
-            
+    .controller('ReclamacoesListCtrl', ['$scope', '$rootScope', '$state', '$stateParams', '$http', 'lodash',
+        function ($scope, $rootScope, $state, $stateParams, $http, lodash) {
+
             var id = Number($stateParams.id);
 
-            if ( !$rootScope.subcategories) {
+            if (!$rootScope.subcategories) {
                 $state.go('app.subCategoriasReclamacoes', { id: id });
+                return;
             }
 
+            $scope.currentSubCategory = lodash.find($rootScope.subcategories, { 'id': id });
+            $scope.currentCategory = lodash.find($rootScope.categories, { 'id': $scope.currentSubCategory.categoryId });
+            /*
             
-/*
-
-            $http({
-                method: 'GET',
-                url: 'http://10.0.0.101:8080/api/getcategorias'
-            }).then(function successCallback(response) {
-                $rootScope.categories = response.data;
-            }, function errorCallback(response) {
-            });
-*/
+                        $http({
+                            method: 'GET',
+                            url: 'http://10.0.0.101:8080/api/getcategorias'
+                        }).then(function successCallback(response) {
+                            $rootScope.categories = response.data;
+                        }, function errorCallback(response) {
+                        });
+            */
 
         }]);
