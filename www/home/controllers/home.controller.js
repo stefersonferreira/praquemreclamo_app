84943242d78a3eb3d786741e23360f9df535a391
Emmanuel Pautet <emmanuel.pautet@gmail.com>
2017-06-11 14:27:08 -0300

    commit

--- www/home/controllers/home.controller.js
+++ www/home/controllers/home.controller.js
@@ -1,92 +1,93 @@
 angular.module('starter')
-    .controller('HomeCtrl', ['$scope', '$rootScope', '$state', '$http', '$ionicModal', 
-    function ($scope, $rootScope, $state, $http, $ionicModal) {
+    .controller('HomeCtrl', ['$scope', '$rootScope', '$state', '$http', '$ionicModal',
+        function ($scope, $rootScope, $state, $http, $ionicModal) {
 
-        $scope.onBtnReclamacao = function () {
-            $state.go('app.categoriasReclamacoes');
-        }
-
-        $scope.onOpenChooseCity = function () {
-            $scope.selectedCity = $scope.currentCity;
-            $scope.modal.show();
-        }
-
-        $scope.searchParams = {
-            searchField: ''
-        }
-
-
-        $scope.onSearchBtn = function() {
-
-            console.log('click')
-            
-            $http({
-                method: 'POST',
-                url: 'http://10.0.0.106:8080/api/buscaresposta/',
-                data: { 'pergunta' : $scope.searchParams.searchField }
-            }).then(function successCallback(response) {
-                console.log(response);
-            }, function errorCallback(response) {
-            });
-        }
-
-
-        $scope.onSpeechRecongition = function () {
-            console.log('click');
-            if ($rootScope.ionicReady) {
-            console.log('reacy');
-                $scope.record = function () {
-                    var recognition = new SpeechRecognition();
-                    recognition.onresult = function (event) {
-                        if (event.results.length > 0) {
-                            $scope.searchParams.searchField = event.results[0][0].transcript;
-                            $scope.$apply()
-                        }
-                    };
-                    recognition.start();
-                };
+            $scope.onBtnReclamacao = function () {
+                $state.go('app.categoriasReclamacoes');
             }
-        }
 
-        /**
-         * MODAL LOCALIZATION
-         */
-        $ionicModal.fromTemplateUrl('my-modal.html', {
-            scope: $scope,
-            animation: 'slide-in-up'
-        }).then(function (modal) {
-            $scope.modal = modal;
-        });
+            $scope.onOpenChooseCity = function () {
+                $scope.selectedCity = $scope.currentCity;
+                $scope.modal.show();
+            }
+
+            $scope.searchParams = {
+                searchField: ''
+            }
 
 
-        $scope.cancelChooseCity = function () {
-            $scope.modal.hide();
-        };
+            $scope.onSearchBtn = function () {
 
-        $scope.selectCity = function (city) {
-            $scope.selectedCity = city;
-        }
+                if ($scope.searchParams.searchField.length > 5) {
+
+                    $http({
+                        method: 'POST',
+                        url: 'http://10.0.0.106:8080/api/buscaresposta',
+                        headers: { 'Content-Type': 'text/plain' },
+                        data: { 'pergunta': $scope.searchParams.searchField }
+                    }).then(function successCallback(response) {
+                        console.log(response);
+                    }, function errorCallback(response) {
+                    });
+                }
+            }
+
+            $scope.onSpeechRecongition = function () {
+                console.log('click');
+                if ($rootScope.ionicReady) {
+                    console.log('reacy');
+                    $scope.record = function () {
+                        var recognition = new SpeechRecognition();
+                        recognition.onresult = function (event) {
+                            if (event.results.length > 0) {
+                                $scope.searchParams.searchField = event.results[0][0].transcript;
+                                $scope.$apply()
+                            }
+                        };
+                        recognition.start();
+                    };
+                }
+            }
+
+            /**
+             * MODAL LOCALIZATION
+             */
+            $ionicModal.fromTemplateUrl('my-modal.html', {
+                scope: $scope,
+                animation: 'slide-in-up'
+            }).then(function (modal) {
+                $scope.modal = modal;
+            });
 
 
-        $scope.saveChooseCity = function () {
-            $scope.modal.hide();
-            $scope.currentCity = $scope.selectedCity;
-        };
+            $scope.cancelChooseCity = function () {
+                $scope.modal.hide();
+            };
+
+            $scope.selectCity = function (city) {
+                $scope.selectedCity = city;
+            }
 
 
-        // Cleanup the modal when we're done with it!
-        $scope.$on('$destroy', function () {
-            $scope.modal.remove();
-        });
-        // Execute action on hide modal
-        $scope.$on('modal.hidden', function () {
-            // Execute action
-        });
-        // Execute action on remove modal
-        $scope.$on('modal.removed', function () {
-            // Execute action
-        });
+            $scope.saveChooseCity = function () {
+                $scope.modal.hide();
+                $scope.currentCity = $scope.selectedCity;
+            };
+
+
+            // Cleanup the modal when we're done with it!
+            $scope.$on('$destroy', function () {
+                $scope.modal.remove();
+            });
+            // Execute action on hide modal
+            $scope.$on('modal.hidden', function () {
+                // Execute action
+            });
+            // Execute action on remove modal
+            $scope.$on('modal.removed', function () {
+                // Execute action
+            });
 
 
 
-    }]);
+        }]);
